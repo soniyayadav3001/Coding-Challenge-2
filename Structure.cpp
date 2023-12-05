b839a3391a38a80bf607c299dbe1ae0ec0b53859
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-05 19:25:21 +0530

    Create Structure.cpp

---
+++ Structure.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+struct vehicle{
+    int gear;
+    int wheels;
+};
+int main() {
+    struct vehicle v[3];
+    cout << "Enter the details here : " << endl;
+    for (int i = 0; i < 3;i++){
+        cin >> v[i].gear >> v[i].wheels;
+    }
+    for (int i = 0; i < 3;i++){
+        cout<<v[i].gear<<v[i].wheels;
+    }
+    
+        return 0;
+}

7141e3f4fef86411b523995ef6bdfe6ab8092017
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-05 19:32:30 +0530

    Create Reverse array.cpp

---
+++ Reverse array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[4];
+    cout << "Enter the elements of the array : " << endl;
+    for (int i = 0; i < 4;i++){
+        cin >> arr[i];
+    }
+    cout << "Array elements are : " << endl;
+    for (int i = 0; i < 4;i++){
+        cout << arr[i];
+    }
+    cout << endl;
+    cout << "Reversed array elements are : " << endl;
+    for (int i = 3; i >= 0;i--){
+        cout<<arr[i];
+    }
+        return 0;
+}

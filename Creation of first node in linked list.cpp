b99bcc814558caf443b4c63dbebfb1112a4c02ea
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2023-12-06 20:02:49 +0530

    Create Creation of first node in linked list.cpp

---
+++ Creation of first node in linked list.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+};
+node *first, *temp, *ttemp;
+void create_first(int val){
+    first = (node *)malloc(sizeof(node));
+    first->data = val;
+    first->next = NULL;
+    }
+
+void display(){
+    temp = first;
+    while(temp!=NULL){
+        cout << temp->data;
+        temp = temp->next;
+    }
+}
+    int main()
+    {
+       create_first(10);
+       cout << "Node has created." << endl;
+       cout << "The value of the data is : " << endl;
+       display();
+    }

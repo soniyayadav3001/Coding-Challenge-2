c555d7a94d88bdadd610e696567d5d3b7b2ccaab
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-01-09 23:31:17 +0530

    Create Virtual Function.cpp

---
+++ Virtual Function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class base
+{
+  public:
+  virtual void show()
+  {
+    cout << "Base class\n";
+  }
+};
+
+class derived:public base
+{
+  public:  void show()
+   {
+     cout << "Derived Class";
+   }
+};
+
+int main()
+{
+  base* p; //Base class pointer
+  derived c; //Derived class object
+  p = &c;
+  c.show(); //Late Binding Occurs
+}

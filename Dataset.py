c8fc7948f01e13e4e4207d4433274a5ebfdc769f
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-09-11 19:13:48 +0530

    Create Dataset.py

---
+++ Dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+df = pd.DataFrame(data)
+print(df)

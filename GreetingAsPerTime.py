fd4cf9a1151aa28ee7807cb34d7a76167354f45b
Soniya Yadav <142081758+soniyayadav3001@users.noreply.github.com>
2024-10-02 21:48:21 +0530

    Update GreetingAsPerTime.py

--- GreetingAsPerTime.py
+++ GreetingAsPerTime.py
@@ -3,7 +3,7 @@ name = input('Enter your name: ')
 recenttime = time.strftime('%H:%M:%S')
 Recenttime = int(time.strftime('%H'))
 c= name.capitalize()
-if(4<=Recenttime<12):
+if(4<=Recenttime<12): 
     print('GOOD MORNING',c,'its',recenttime)
 elif(12<=Recenttime<=15):
     print('GOOD AFTERNOON',c,'its',recenttime)
